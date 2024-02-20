1ffd4a0e9eff7f119f9e3bfb6c36c04f3e8fcfaa
m-fahad-nasir <fahad.korea.jbnu@gmail.com>
2024-02-20 15:42:06 +0900

    2nd Commit

--- Tester.py
+++ Tester.py
@@ -1,3 +1,17 @@
-x = 7
-y = 1992
+# CWC Captain Identifier
+y = 2024
 print('Pakistan Won World Cup in :', y)
+
+if (y == 1992):
+    cap = "Imran Khan"
+
+elif (y == 2000):
+    cap = 'Wasim Akram'
+
+elif (y == 2007):
+    cap = 'Inzamam ul Haq'
+
+else:
+    cap = 'Babar Azam'
+
+print('The Winning Captain was ', cap)
